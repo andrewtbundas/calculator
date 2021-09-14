ee374dc0ffc353fbe7945b3a678aac72fa6db943
andrewtbundas <andrewtbundas@gmail.com>
2021-09-14 14:59:46 -0400

    Basic calculator functioning, need to edit to work with multiple inputs at once, save previous input for next operation?

--- script.js
+++ script.js
@@ -1,19 +1,79 @@
-const add = function (x, y) {
+const buttons = document.querySelectorAll('.calc-button');
+buttons.forEach((button) => button.addEventListener('click', () => {
+    console.log('we did a click on ' + button.getAttribute('data-key'))
+    displayUpdate(button.getAttribute('data-key'));
+}))
+
+const clear = document.getElementById('clear')
+const equals = document.getElementById('equals')
+let x = "";
+let y = "";
+let operation = "";
+
+
+function executeFunctionByName(functionName, context /*, args */) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    var namespaces = functionName.split(".");
+    var func = namespaces.pop();
+    for (var i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]];
+    }
+    return context[func].apply(context, args);
+}
+
+function add(x, y) {
     return x + y
 }
 
-const subtract = function (x, y) {
+function subtract(x, y) {
     return x - y
 }
 
-const multiply = function (x, y) {
+function multiply(x, y) {
     return x * y
 }
 
-const divide = function (x, y) {
+function divide(x, y) {
     return x / y
 }
 
 const operate = function (operator, x, y) {
-    return operator(x, y)
+    x = Number(x);
+    y = Number(y);
+    let fn = window[operator]
+    return fn(x, y)
 }
+
+const displayUpdate = function (input) {
+    if (input == 'add' || input == 'multiply' || input == 'divide' || input == 'subtract') {
+        operation = input;
+    }
+    else {
+        if (!operation) {
+            x += input
+        }
+        else {
+            y += input
+        }
+    }
+    let displayText = `${x} ${operation} ${y}`
+    document.getElementById("display").innerHTML = displayText;
+}
+
+const clearCalculator = function () {
+    x = ''
+    y = ''
+    operation = ''
+    displayText = ''
+    document.getElementById("display").innerHTML = displayText;
+}
+
+clear.addEventListener('click', clearCalculator)
+equals.addEventListener('click', () => {
+    let result = operate(operation, x, y);
+    document.getElementById("display").innerHTML = result;
+    console.log(result);
+})
+
+
+
