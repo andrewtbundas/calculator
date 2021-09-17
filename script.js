18d130f44a0e452b1664cf5c180ba75d425aff11
andrewtbundas <andrewtbundas@gmail.com>
2021-09-17 12:28:38 -0400

    fixed chaining of results, still need to add backspace capability

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttons = document.querySelectorAll('.calc-button');
 buttons.forEach((button) => button.addEventListener('click', () => {
-    console.log('we did a click on ' + button.getAttribute('data-key'))
+    //console.log('we did a click on ' + button.getAttribute('data-key'))
     displayUpdate(button.getAttribute('data-key'));
 }))
 
@@ -9,6 +9,7 @@ const equals = document.getElementById('equals')
 let x = "";
 let y = "";
 let operation = "";
+let result = '';
 
 function add(x, y) {
     return x + y
@@ -26,6 +27,26 @@ function divide(x, y) {
     return x / y
 }
 
+function operationTranslate(str){
+    if (str == 'add'){
+        return '+'
+    }
+    else if (str =='multiply'){
+        return 'x'
+    }
+
+    else if (str == 'divide'){
+        return '&divide'
+    }
+    else if (str == 'subtract'){
+        return '-'
+    }
+
+    else{
+        return ''
+    }
+}
+
 const operate = function (operator, x, y) {
     x = Number(x);
     y = Number(y);
@@ -37,7 +58,15 @@ const operate = function (operator, x, y) {
 const displayUpdate = function (input) {
     if (input == 'add' || input == 'multiply' || input == 'divide' || input == 'subtract') {
         operation = input;
+        result = ''
     }
+    
+    else if (result){
+        //console.log('we ahve a result');
+        result = ''
+        x = input;
+    }
+
     else {
         if (!operation) {
             x += input
@@ -46,7 +75,10 @@ const displayUpdate = function (input) {
             y += input
         }
     }
-    let displayText = `${x} ${operation} ${y}`
+
+
+
+    let displayText = `${x} ${operationTranslate(operation)} ${y}`
     document.getElementById("display").innerHTML = displayText;
 }
 
@@ -55,6 +87,7 @@ const clearCalculator = function () {
     y = ''
     operation = ''
     displayText = ''
+    result = ''
     document.getElementById("display").innerHTML = displayText;
 }
 
@@ -66,12 +99,12 @@ equals.addEventListener('click', () => {
             document.getElementById("display").innerHTML = "Can't divide by zero, you fool";
         }
         else {
-            let result = Math.round(1e4 * operate(operation, x, y)) / 1e4;
+            result = Math.round(1e4 * operate(operation, x, y)) / 1e4;
             x = result;
             y = ''
             operation = ''
             document.getElementById("display").innerHTML = result;
-            console.log(result);
+            //console.log(result);
         }
 
 
